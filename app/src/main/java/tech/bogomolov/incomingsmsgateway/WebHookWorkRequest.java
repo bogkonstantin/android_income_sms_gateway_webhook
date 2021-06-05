c1c226e1383ce6efacad89d2062448d2911ab913
Konstantin Bogomolov <konstantin@bogomolov.tech>
2021-06-05 14:59:57 +0530

    Set http headers

--- app/src/main/java/tech/bogomolov/incomingsmsgateway/WebHookWorkRequest.java
+++ app/src/main/java/tech/bogomolov/incomingsmsgateway/WebHookWorkRequest.java
@@ -66,6 +66,8 @@ public class WebHookWorkRequest extends Worker {
             urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setDoOutput(true);
             urlConnection.setChunkedStreamingMode(0);
+            urlConnection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+            urlConnection.setRequestProperty("User-agent", "SMS Forwarder App");
 
             OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8));
